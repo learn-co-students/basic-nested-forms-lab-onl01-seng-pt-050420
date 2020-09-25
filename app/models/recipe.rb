743c40e769782febb4d3c4d3d1268aac53d84eeb
ValentinaPanic <valentina.panic87@gmail.com>
2020-09-24 18:29:10 -0600

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,13 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  def ingredient_attributes=(ingredient)
+    self.ingredient = Ingredient.find_or_create_by(name: ingredient[:name])
+    if self.ingredient.name != nil && self.ingredient.quantity != nil
+    self.ingredient.update(ingredient)
+    end
+
+    
+  end
 end
